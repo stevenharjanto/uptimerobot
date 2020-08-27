ed2ffc72e0ec2f282f11515aae9c2fccd7edb219
stevenharjanto <69180534+stevenharjanto@users.noreply.github.com>
2020-08-27 13:07:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://mtbbot.glitch.me"]
+const urls = ["https://mtbzoom.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
